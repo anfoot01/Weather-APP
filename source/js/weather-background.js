8edef5ab294cdcf75f5610995533207acc9c19cc
Anton Zinoviev <avz010308@gmail.com>
2024-06-05 20:49:26 +0200

    fixed bag with background

--- source/js/weather-background.js
+++ source/js/weather-background.js
@@ -1,27 +1,27 @@
 //WEATHER BACKGROUND FUNCTION
 
 function thunderBg(){
-    card.style.background = "url(../../img/thunderstorm.webp)"
+    card.style.backgroundImage = "url(../../img/thunderstorm.webp)"
     card.style.color = "white";
   }
   function rainBg(){
-    card.style.background = "url(../../img/rain.jpg)";
+    card.style.backgroundImage = "url(../../img/rain.jpg)";
     card.style.color = "white";
   }
   function cloudBg(){
-    card.style.background = "url(../../img/clouds.jpg)";
+    card.style.backgroundImage = "url(../../img/clouds.jpg)";
     card.style.color = "black";
   }
   function fogBg(){
-    card.style.background = "url(../../img/fog.png)";
+    card.style.backgroundImage = "url(../../img/fog.png)";
     card.style.color = "black";
   }
   function sunBg(){
-    card.style.background = "url(../../img/sun.jpg)";
+    card.style.backgroundImage = "url(../../img/sun.jpg)";
     card.style.color = "black";
   }
   function snowBg(){
-    card.style.background = "url(../../img/snow.png)";
+    card.style.backgroundImage = "url(../../img/snow.png)";
     card.style.color = "black";
   }
   
